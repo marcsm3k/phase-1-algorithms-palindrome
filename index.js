9f27510f95925bacc79d4f221ac38390bd558e7d
Marco <marcovelardejr@gmail.com>
2022-06-04 13:15:50 -0500

    commit tree ccb58381fd10d254006fd3e6be63f9c9f8d1fbea

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const string = word.toLowerCase()
+  const reversestring = string.split('').reverse().join('')
+
+  if (string === reversestring) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+
+  Initialize function called isPalindrome that uses word as argument
+
+  declare variable for argument: make all letters lowercase
+
+  make every letter in into array
+  
+  declare another variable: reverse order of each element in the array
+
+  join each element in array
+
+  if variable 1 equals variable 2
+  return true 
+
+  else
+  return false
+
 */
+
+
 
 /*
   Add written explanation of your solution here
